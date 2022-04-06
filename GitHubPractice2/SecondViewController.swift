1e9713342236f1799551994b03f29876e17feeae
Nathan Kim <nkim3215@stu.d214.org>
2022-04-06 12:54:20 -0500

    animal variable

--- GitHubPractice2/SecondViewController.swift
+++ GitHubPractice2/SecondViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var animal: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        animal = nameLabel.text
+        imageView.image = UIImage(named: "animal")
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
