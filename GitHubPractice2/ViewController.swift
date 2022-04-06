c1a2cca7b5ec40d43065aa55d2d467da11ad433d
Nathan Kim <nkim3215@stu.d214.org>
2022-04-06 12:28:55 -0500

    Initial Commit

---
+++ GitHubPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice2
+//
+//  Created by Nathan Kim on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
